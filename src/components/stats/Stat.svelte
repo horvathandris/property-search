3f888ed6ba1b1479216089e8ca180781652ecd93
Horváth Andris <horvath.andris@wise.com>
2025-08-17 23:21:03 +0100

    Add files via upload
    Signed-off-by: Horváth Andris <horvath.andris@wise.com>

--- src/components/stats/Stat.svelte
+++ src/components/stats/Stat.svelte
@@ -1,10 +1,24 @@
 <script lang="ts">
     let { title, value, color = "blue" } = $props();
+    const colors = {
+        "blue": {
+            bg: "bg-blue-100",
+            text: "text-blue-700",
+        },
+        "green": {
+            bg: "bg-green-100",
+            text: "text-green-700",
+        },
+        "red": {
+            bg: "bg-red-100",
+            text: "text-red-700",
+        },
+    };
 </script>
 
 <div class="stats">
-    <div class={["stat", `bg-${color}-50`]}>
+    <div class={["stat", colors[color].bg]}>
         <div class="stat-title">{title}</div>
-        <div class={["stat-value", `text-${color}-500`]}>£{value.toLocaleString()}</div>
+        <div class={["stat-value", colors[color].text]}>£{value.toLocaleString()}</div>
     </div>
 </div>
